d41fc9b71ae4fd6a79b50bab411ec0454a1feb11
前端架构师 <62106726+qdleader@users.noreply.github.com>
2023-12-28 22:27:36 +0800

    feat: init pro

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress"
 
 export default defineUserConfig({
 	lang: "zh-CN",
-	title: "你好， VuePress ！",
+	title: "你好！",
 	description: "这是我的第一个 VuePress 站点",
 	themeConfig: {
 		// sidebar: require('./layout/sideBarCon'),
