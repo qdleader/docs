5eb4262262d475b78348a7bde7bec9e981b7b87f
前端架构师 <62106726+qdleader@users.noreply.github.com>
2023-12-28 22:52:30 +0800

    feat: init pro1

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress"
 
 export default defineUserConfig({
 	lang: "zh-CN",
-	title: "你好",
+	title: "你",
 	description: "qdleader",
 	themeConfig: {
 		// sidebar: require('./layout/sideBarCon'),
