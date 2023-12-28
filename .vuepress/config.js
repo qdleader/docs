51c70af6f1770603a2724b0f4c10cf15269c52c2
yangkun <yangkun@icourt.cc>
2023-12-28 17:46:51 +0800

    feat: init project

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: '你好， VuePress ！',
+    description: '这是我的第一个 VuePress 站点',
+    themeConfig: {
+        sidebar: require('./layout/sideBarCon'),
+    }
+})
---
+++ .vuepress/layout/sideBarCon.js
@@ -0,0 +1,5 @@
+const getDocPath = require('./getDocPath')
+module.exports = [
+    getDocPath('vue',true,'/npm/'),
+    getDocPath('js',true,'/支付/')
+];
