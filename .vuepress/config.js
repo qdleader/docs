59b9192146a22e618048006e8a80b67d241b5f67
前端架构师 <62106726+qdleader@users.noreply.github.com>
2024-01-01 16:28:13 +0800

    feat: init pro57

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,6 +5,7 @@ import { autoCatalogPlugin } from "vuepress-plugin-auto-catalog"
 
 export default defineUserConfig({
 	lang: "zh-CN",
+	base: "/docs/",
 	title: "你好， qdleader ！",
 	description: "qdleader,前端面试题大全, 编程学习路线, 编程知识百科, Java, 编程导航, 前端, 开发, 编程分享, 项目, IT, 求职, 面经",
 	// plugins: [
