01b30ff8cc92d9a983e6cc637c6d14faaaaafe83
前端架构师 <yangkun@icourt.cc>
2024-01-02 14:32:25 +0800

    feat: 增加footer 布局

---
+++ .vuepress/layouts/layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <ParentLayout>
+    <template #page-content-bottom>
+      <div>
+        <!-- <div class="money">打赏</div> -->
+        <!-- <div>评论</div> -->
+      </div>
+    </template>
+    <template #page-bottom>
+      <div class="footer">
+        <a href="https://github.com/qdleader/qdleader">qdleader</a>
+      </div>
+    </template>
+  </ParentLayout>
+</template>
+<script>
+import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
+export default {
+  components: {
+    ParentLayout,
+  },
+};
+</script>
+<style>
+.footer {
+  width: 100%;
+  text-align: center;
+}
+</style>
