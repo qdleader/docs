adb1da5341f4490c7b770485faad0e61e54786ae
前端架构师 <yangkun@icourt.cc>
2024-01-02 19:38:55 +0800

    init: base Num

--- .vuepress/layouts/layout.vue
+++ .vuepress/layouts/layout.vue
@@ -13,12 +13,12 @@
         <div class="busuanzi">
           <span id="busuanzi_container_site_pv">
             本站总访问量
-            <span id="busuanzi_value_site_pv"></span>次
+            <span id="busuanzi_value_site_pv">129823</span>次
             <span class="post-meta-divider">|</span>
           </span>
           <span id="busuanzi_container_site_uv">
             本站访客数
-            <span id="busuanzi_value_site_uv"></span>人
+            <span id="busuanzi_value_site_uv">12</span>人
           </span>
         </div>
       </div>
